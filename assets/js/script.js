67d682ea3a92e75654f41b2bdf8f1926e276ecb9
hossein nadi <ho03inna76@gmail.com>
2024-01-19 22:17:58 +0330

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,17 @@ overlay.classList.remove("active")
 }
 addEventOnElem(navbarLinks, "click" , closeNavbar)
 /*
-header sticky
+header sticky & back top btn active
   */
  const header = document.querySelector("[data-header]")
+ const backTopbtn = document.querySelector("[data-back-top-btn]")
  const headerActive = function() {
     if (window.scrollY > 100){
         header.classList.add("active")
+        backTopbtn.classList.add("active")
     }   else {
         header.classList.remove("active")
+        backTopbtn.classList.remove("active")
     }
  }
  addEventOnElem(window , "scroll" , headerActive)
@@ -50,4 +53,17 @@ header sticky
     lastScrolledpos = window.scrollY
  }
  addEventOnElem(window , "scroll" , headerSticky)
- 
+ /*
+    scroll reveal effect
+  */
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function (){
+    for (let i = 0; i < sections.length; i++){
+        if (sections [i].getBoundingClientRect().top < window.innerHeight / 2){
+            sections [i].classList.add("active");
+        }
+    }
+}
+scrollReveal();
+addEventOnElem(window, "scroll" , scrollReveal);
